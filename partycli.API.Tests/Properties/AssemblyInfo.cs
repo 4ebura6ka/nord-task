31b417befdf3cef33cfcbba7ed2de4ce4f3914fa
Sergej Poltorak <sergej.poltorak@gmail.com>
2023-09-27 00:21:56 +0300

    Add test project

---
+++ partycli.API.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("partycli.API.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("partycli.API.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38567f9f-b8bf-4c54-b9ea-2aa3b37356e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ partycli.API.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PartyCli.API;
+using System;
+using System.Net.Http;
+
+namespace partycli.API.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var httpClient = new HttpClient();
+            var serverService = new ServerService();
+
+        }
+    }
+}
